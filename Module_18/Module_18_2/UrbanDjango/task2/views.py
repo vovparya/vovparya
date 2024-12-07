0f43edff1629c409cc8873825f8187ce92cea415
vovparya <vovparya@gmail.com>
2024-12-07 18:27:47 +0300

    Задания модуля 18. Домашнее задание по теме "Urls и Views. Функциональное и классовое представление."

---
+++ Module_18/Module_18_2/UrbanDjango/task2/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+
+
+# Функциональное представление
+def function_based_view(request):
+    return render(request, 'second_task/function_view.html')
+
+
+# Классовое представление
+class ClassBasedView(View):
+    def get(self, request):
+        return render(request, 'second_task/class_view.html')
