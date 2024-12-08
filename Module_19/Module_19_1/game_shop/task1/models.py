14298ab5237f84f2241bbc65763d82840a74b67e
vovparya <vovparya@gmail.com>
2024-12-08 13:08:31 +0300

    Задания модуля 19. Домашнее задание по теме "Модели баз данных в Django."

---
+++ Module_19/Module_19_1/game_shop/task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+
+    def __str__(self):
+        return self.title
